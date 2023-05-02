04fc42736f9bcaa609b2667dbbb1b803973663eb
Akio <akio@akios-mbp.speedport.ip>
2023-05-02 11:31:08 +0200

    create working main file, fix openAI key

--- Googledownload.py
+++ Googledownload.py
@@ -6,3 +6,48 @@ from google.auth.transport.requests import Request
 
 from google_auth_oauthlib.flow import InstalledAppFlow
 from llama_index import GPTSimpleVectorIndex, download_loader
+
+def authorize_gdocs():
+    google_oauth2_scopes = [
+        "https://www.googleapis.com/auth/documents.readonly"
+    ]
+    cred = None
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", 'rb') as token:
+            cred = pickle.load(token)
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", google_oauth2_scopes)
+            cred = flow.run_local_server(port=0)
+        with open("token.pickle", 'wb') as token:
+            pickle.dump(cred, token)
+
+
+# function to authorize or download latest credentials 
+authorize_gdocs()
+
+# initialize LlamaIndex google doc reader 
+GoogleDocsReader = download_loader('GoogleDocsReader')
+
+# list of google docs we want to index 
+gdoc_ids = ['16UhCeZMj3N-VS31arHOgbILqg1Eboozbj2kps4BIgd8']
+
+loader = GoogleDocsReader()
+
+# load gdocs and index them 
+documents = loader.load_data(document_ids=gdoc_ids)
+index = GPTSimpleVectorIndex(documents)
+
+# Save your index to a index.json file
+index.save_to_disk('index.json')
+# Load the index from your saved index.json file
+index = GPTSimpleVectorIndex.load_from_disk('index.json')
+
+# Querying the index
+while True:
+    prompt = input("Type prompt...")
+    response = index.query(prompt)
+    print(response)
+    
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import llama_index
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+#import dotenv
+#from dotenv import load_dotenv
+#load_dotenv()
+import os
+
+os.environ['OPENAI_API_KEY'] = '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'
+token = os.environ.get("OPEN-API-KEY")
+
 
 # Define the path to your data directory
-data_dir = "/path/to/data/directory"
+data_dir = "/Users/akio/Documents/TestChatRfP"
 
 # Create a new LlamaIndex
-index = llama_index.Index()
-
-# Define the file formats you want to index
-file_formats = ["docx", "xlsx", "pdf", "txt"]
-
-# Index the documents in your data directory
-for file_format in file_formats:
-    index.add_directory(data_dir, file_format)
+documents = SimpleDirectoryReader (data_dir).load_data()
+index = llama_index.GPTSimpleVectorIndex.from_documents(documents)
 
 # Search for documents in the index
-results = index.search("your search query")
-for result in results:
+#results = index.search("your search query")
+#for result in results:
     # Print the file path and score for each result
-    print(result["path"], result["score"])
+    #print(result["path"], result["score"])
+
+# load gdocs and index them 
+#documents = loader.load_data(document_ids=gdoc_ids)
+#whatindex = GPTSimpleVectorIndex(documents)
+
+# Save your index to a index.json file
+index.save_to_disk('index.json')
+# Load the index from your saved index.json file
+index = GPTSimpleVectorIndex.load_from_disk('index.json')
+
+# Querying the index
+while True:
+    prompt = input("Type prompt...")
+    response = index.query(prompt)
+    print(response)
+    
